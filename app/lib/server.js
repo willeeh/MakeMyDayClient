a9c06685a0fca634d152a63d1a7b9f32d6049056
Guillermo Ceballos <willeeh@gmail.com>
2013-01-20 17:16:50 +0100

    Adding some stuff

---
+++ app/lib/server.js
@@ -0,0 +1,29 @@
+//*** Public properties:
+exports.SIGNED = 'signed';
+
+exports.POST  	= 'POST';
+exports.GET 	= 'GET';
+ 
+//*** Protected variables:
+var host = "http://localhost:8080/v1/";
+ 
+//*** Public methods:
+exports.signUp = function (userData) {
+    send('user', exports.POST, JSON.stringify(userData));
+};
+ 
+//*** Protected method used only internally.
+send = function (path, method, postData) {
+	var xhr = Ti.Network.createHTTPClient();
+
+	xhr.onload = function(e) {
+		Ti.App.fireEvent(exports.SIGNED,e);
+	};
+			
+	xhr.open(method, host+method);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	if (postData)
+		xhr.send(postData);
+	else
+		xhr.send();        
+};
